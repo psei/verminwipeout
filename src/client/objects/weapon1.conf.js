0cd7b62b45180b875f6fb17679abc192afd24ea8
Mathias Albrecht <ma@classwizard.com>
2017-04-19 15:08:56 +0200

    add weapon config

---
+++ src/client/objects/weapon1.conf.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var config = require('../baseConfig');
+
+module.exports = config({
+  images: {
+    bullet: '/images/shots/shot-1.png'
+  },
+  reloadTime: 200,
+  fireLimit: 30,
+  bulletSpeed: 720
+});

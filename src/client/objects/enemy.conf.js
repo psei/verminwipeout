637736aa5ce7d0c3d18fbb74bd1576d206e07a32
pseikow <pseikow@gmail.com>
2017-04-29 08:34:47 +0200

    rename enemies

--- src/client/objects/enemy.conf.js
+++ src/client/objects/enemy.conf.js
@@ -6,7 +6,7 @@ module.exports = config({
   sprites: {
     enemy: {
       animationName: 'enemy-1-fly',
-      imageSrc: '/images/enemies/enemy-1.png',
+      imageSrc: '/images/enemies/cutterfly/fly.png',
       frames: [1, 2],
       width: 80,
       height: 100,
@@ -14,7 +14,7 @@ module.exports = config({
     },
     death: {
       animationName: 'enemy-1-death',
-      imageSrc: '/images/enemies/enemy-1-death.png',
+      imageSrc: '/images/enemies/cutterfly/death.png',
       frames: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
       width: 410,
       height: 300,
@@ -22,7 +22,7 @@ module.exports = config({
     },
     attack: {
       animationName: 'enemy-1-attack',
-      imageSrc: '/images/enemies/enemy-1-attack.png',
+      imageSrc: '/images/enemies/cutterfly/attack.png',
       frames: [1, 2],
       width: 80,
       height: 100,

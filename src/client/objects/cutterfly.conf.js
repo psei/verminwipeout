15309c14d199eaee7431e54fd3bafca6530cd6e2
David Edler <david@77177.de>
2019-08-02 20:14:12 +0200

    reduce death sprite size to fix mobile animations (chrome android only loads images less than 4096 pixel width)

--- src/client/objects/cutterfly.conf.js
+++ src/client/objects/cutterfly.conf.js
@@ -7,7 +7,7 @@ module.exports = config({
     enemy: {
       animationName: 'cutterfly-fly',
       imageSrc: '/images/enemies/cutterfly/fly.png',
-      frames: [1, 2],
+      frames: [0, 1],
       width: 80,
       height: 100,
       frameRate: 5
@@ -15,15 +15,15 @@ module.exports = config({
     death: {
       animationName: 'cutterfly-death',
       imageSrc: '/images/enemies/cutterfly/death.png',
-      frames: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-      width: 410,
-      height: 300,
+      frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+      width: 333,
+      height: 244,
       frameRate: 12
     },
     attack: {
       animationName: 'cutterfly-attack',
       imageSrc: '/images/enemies/cutterfly/attack.png',
-      frames: [1, 2],
+      frames: [0, 1],
       width: 80,
       height: 100,
       frameRate: 3
--- src/client/objects/jizzler.conf.js
+++ src/client/objects/jizzler.conf.js
@@ -7,7 +7,7 @@ module.exports = config({
     enemy: {
       animationName: 'jizzler-fly',
       imageSrc: '/images/enemies/jizzler/fly.png',
-      frames: [1, 2],
+      frames: [0, 1],
       width: 34,
       height: 98,
       frameRate: 5
@@ -15,15 +15,15 @@ module.exports = config({
     death: {
       animationName: 'jizzler-death',
       imageSrc: '/images/enemies/cutterfly/death.png',
-      frames: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-      width: 410,
-      height: 300,
+      frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      width: 333,
+      height: 244,
       frameRate: 12
     },
     attack: {
       animationName: 'jizzler-attack',
       imageSrc: '/images/enemies/jizzler/attack.png',
-      frames: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+      frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
       width: 34,
       height: 98,
       frameRate: 10
